d4ab93ccbb6fadf2b7eecd30295851ce29987267
Smritikona Sukul <ssukul9@gmail.com>
2025-10-15 16:47:48 +0530

    Added files

---
+++ Model/ProductDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAappProject.Model
+{
+    public class ProductDetails
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string ProductType { get; set; }
+    }
+}
